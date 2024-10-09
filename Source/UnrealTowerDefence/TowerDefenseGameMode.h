365cc0d8ec8bbe5763d66a1b3b4e7e4bc8410bf1
SeabassOlko <63417711+SeabassOlko@users.noreply.github.com>
2024-10-09 16:07:21 -0500

    Started added scripts and Pawns

---
+++ Source/UnrealTowerDefence/TowerDefenseGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerDefenseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTOWERDEFENCE_API ATowerDefenseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	//added memes
+
+};
